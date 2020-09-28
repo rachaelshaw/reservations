40516ee6b97c6733b1184a6ceb5abb40d6f997b1
Rachael Shaw <r@rachael.wtf>
2020-09-27 22:31:32 -0500

    Just list all upcoming reservations on the logged-in homepage to start with

--- api/controllers/dashboard/view-reservations-overview.js
+++ api/controllers/dashboard/view-reservations-overview.js
@@ -17,9 +17,23 @@ module.exports = {
 
 
   fn: async function () {
+    var moment = require('moment-timezone');
+
+    // TODO: make a helper to fetch the reservation schedule for a specific day,
+    // instead of dumbly grabbing all of them here.
+    let reservations = await Reservation.find({restaurant: this.req.me.restaurant})
+    .sort('startsAt ASC');
+
+    reservations = reservations.map((reservation)=> {
+      reservation.startDate = moment(reservation.startsAt).tz(this.req.myRestaurant.tz).format('MM/DD');
+      reservation.startTime = moment(reservation.startsAt).tz(this.req.myRestaurant.tz).format('hh:mma');
+      return reservation;
+    });
 
     // Respond with view.
-    return {};
+    return {
+      reservations
+    };
 
   }
 
